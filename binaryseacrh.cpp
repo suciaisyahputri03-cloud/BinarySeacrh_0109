eab501043addcf0c562622888f818255d9b24a31
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-16 10:26:18 +0700

    menambahkan function

--- binaryseacrh.cpp
+++ binaryseacrh.cpp
@@ -4,3 +4,16 @@ using namespace std;
 int element[10];
 int panjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elmen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if(nPanjang <= 10)
+        {
+            break;
+        }
+}
