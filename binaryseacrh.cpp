17a7634f46c9761fc54a29644f483098a411b789
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-04-16 10:44:58 +0700

    menambahkan bublesort array

--- binaryseacrh.cpp
+++ binaryseacrh.cpp
@@ -32,3 +32,20 @@ void input()
         cin >> element[i];
     }
 }
+
+void bubleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int tempt = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = tempt;
+            }
+        }
+    }
+}
